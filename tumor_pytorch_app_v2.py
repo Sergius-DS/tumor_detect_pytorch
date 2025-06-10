ec0ba522666ca676d14167188da02cf9e15414aa
Sergius-DS <sergioskhy@gmail.com>
2025-06-10 09:43:06 -0400

    Update color image font

--- tumor_pytorch_app_v2.py
+++ tumor_pytorch_app_v2.py
@@ -123,7 +123,13 @@ with col1:
 with col2:
     if uploaded_file:
         image = Image.open(uploaded_file).convert('RGB')
-        st.image(image, caption='Imagen cargada.', width=240)
+        # Display image
+        st.image(image, width=240)
+        # Add custom caption with white font
+        st.markdown(f"<p style='color: white; font-weight: bold;'>Imagen cargada.</p>", unsafe_allow_html=True)
+    #if uploaded_file:
+        #image = Image.open(uploaded_file).convert('RGB')
+        #st.image(image, caption='Imagen cargada.', width=240)
 
 # Predicción
 if predict_button and uploaded_file:
